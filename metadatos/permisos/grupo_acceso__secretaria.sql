2c146f4d06cc87a42ab174260ccc6fed3c0a5f33
pkogan <pkogan@pkogan-Lenovo-B590>
2017-06-19 13:17:33 -0300

    Instructivo valor de viaticos a $900

--- metadatos/permisos/grupo_acceso__secretaria.sql
+++ metadatos/permisos/grupo_acceso__secretaria.sql
@@ -110,6 +110,12 @@ INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, i
 	'descentralizado', --proyecto
 	'secretaria', --usuario_grupo_acc
 	NULL, --item_id
+	'3723'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'descentralizado', --proyecto
+	'secretaria', --usuario_grupo_acc
+	NULL, --item_id
 	'3724'  --item
 );
 --- FIN Grupo de desarrollo 0
