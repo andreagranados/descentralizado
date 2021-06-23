6b9fc6aa54d8a9714f6d3a39240aaac127a8ad9c
Andrea <Andrea@LAPTOP-CK2T37FA>
2021-06-23 11:43:35 -0300

    Agrego Total x Inciso (para Presupuesto) para todos lo periodos no solo el actual

--- php/datos/dt_mocovi_descentralizado_presupuesto_general.php
+++ php/datos/dt_mocovi_descentralizado_presupuesto_general.php
@@ -90,11 +90,7 @@ group by pc.id_periodo,pc.id_unidad,pc.id_programa,pc.id_tipo_gasto,pc.id_fuente
     }
     function get_totalesxinciso($where=null) {
         $id_periodo_actual = php_mocovi::instancia()->periodo_a_presupuestar();
-        if (is_null($where)) {
-            $where = 'where t_p.id_periodo='.$id_periodo_actual;
-        } else {
-            $where = ' where ' . $where;
-        }
+        
         $sql="delete from mocovi_descentralizado_presupuesto_general where id_periodo=$id_periodo_actual   and id_objeto_del_gasto in (4,5,6,80);"
                 . "insert into mocovi_descentralizado_presupuesto_general (id_periodo,id_unidad,id_programa, id_tipo_gasto, id_fuente,inciso,id_objeto_del_gasto,monto)
   Select pc.id_periodo,pc.id_unidad,pc.id_programa,pc.id_tipo_gasto,pc.id_fuente,
@@ -134,6 +130,13 @@ group by pc.id_periodo,pc.id_unidad,pc.id_programa,pc.id_tipo_gasto,pc.id_fuente
 ) pc
 inner join mocovi_costo_categoria cc on pc.id_periodo=cc.id_periodo and pc.id_categoria=cc.codigo_siu
 group by pc.id_periodo,pc.id_unidad,pc.id_programa,pc.id_tipo_gasto,pc.id_fuente,pc.id_escalafon;";
+        toba::db('descentralizado')->ejecutar($sql);
+        
+        if (is_null($where)) {
+            $where = 'where t_p.id_periodo='.$id_periodo_actual;
+        } else {
+            $where = ' where ' . $where;
+        }
         
         $sql="select inciso, codigo_completo, nombre, sum(case when id_tipo_gasto=1 then  monto else 0 end) as se, sum(case when id_tipo_gasto=2 then  monto else 0 end) as na, sum(monto) as total
 from(
@@ -161,8 +164,8 @@ select sub2.nombre as inciso,id_tipo_gasto,tipo_gasto,sub1.id_periodo_nombre,id_
                         inner join mocovi_programa as t_pp on (t_p.id_programa = t_pp.id_programa)
                         inner join mocovi_descentralizado_tipo_gasto as t_tg on (t_p.id_tipo_gasto=t_tg.id_tipo_gasto)
 			inner JOIN mocovi_descentralizado_objeto_del_gasto as t_odg ON (t_p.id_objeto_del_gasto = t_odg.id_objeto_del_gasto)
-                        Left outer join mocovi_descentralizado_objeto_del_gasto as t_odg2 on t_odg.elemento_padre=t_odg2.elemento
-                        where  t_p1.id_periodo=".$id_periodo_actual
+                        Left outer join mocovi_descentralizado_objeto_del_gasto as t_odg2 on t_odg.elemento_padre=t_odg2.elemento"
+                        .$where
                         ." order by t_p.id_unidad,t_odg.codigo_completo,tipo_gasto)sub1
                         left outer join (SELECT elemento as inciso,
                                         codigo_completo,
